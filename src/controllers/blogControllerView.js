5a9d83a4cc00ee3c5e1659ece44ed7998b9cd9e4
Ekrem <avekremyilmazturk@gmail.com>
2023-11-09 13:04:38 +0300

    up

--- src/controllers/blogControllerView.js
+++ src/controllers/blogControllerView.js
@@ -1,9 +1,5 @@
 "use strict"
-/* -------------------------------------------------------
-    EXPRESSJS - BLOG Project with Mongoose
-------------------------------------------------------- */
-// https://mongoosejs.com/docs/queries.html
-
+/* ------------------------------------------------------- */
 // Catch async-errors and send to errorHandler:
 require('express-async-errors')
 
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,5 @@
 "use strict"
-/* -------------------------------------------------------
-    EXPRESSJS - BLOG Project with Mongoose
-------------------------------------------------------- */
+/* ------------------------------------------------------- */
 
 // Catch async-errors and send to errorHandler:
 require('express-async-errors')
